1f12e9abf65aa02954152e7ff8506f689d981f10
Wojciech Dutkiewicz <dutwoj95@gmail.com>
2023-03-29 13:27:01 +0200

    ok

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,22 @@
 
 // // list.innerHTML = item;
 
+// const list = document.getElementById('ingredients');
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+// for (let i = 0; i < ingredients.length; i++) {
+//   const item = document.createElement('li');
+//   item.textContent = ingredients[i];
+//   list.append(item);
+//   item.classList.add('item');
+// }
 const list = document.getElementById('ingredients');
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
-for (let i = 0; i < ingredients.length; i++) {
-  const item = document.createElement('li');
-  item.textContent = ingredients[i];
-  list.append(item);
-  item.classList.add('item');
+function addItem(...args) {
+  for (const ingredient of ingredients) {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    list.append(item);
+    item.classList.add('item');
+  }
 }
+addItem(ingredients);
