5817b23df6b7f98a3608ff0f5d4011732f0312b3
Wojciech Dutkiewicz <dutwoj95@gmail.com>
2023-03-31 13:42:10 +0200

    ok

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,16 @@
+let list = document.getElementById('ingredients');
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const frag = document.createDocumentFragment();
+console.log(frag);
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = ingredient;
+  frag.append(item);
+});
+
+list.append(frag);
 // const list = document.querySelector('ul');
 
 // const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
@@ -14,17 +27,30 @@
 //   list.append(item);
 //   item.classList.add('item');
 // }
-const list = document.getElementById('ingredients');
-const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
-function addItem(...args) {
-  for (const ingredient of ingredients) {
-    const item = document.createElement('li');
-    item.textContent = ingredient;
-    list.append(item);
-    item.classList.add('item');
-  }
-}
-addItem(ingredients);
+
+// const list = document.getElementById('ingredients');
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+// function addItem(array) {
+//   for (const element of array) {
+//     const item = document.createElement('li');
+//     item.textContent = element;
+//     list.append(item);
+//     item.classList.add('item');
+//   }
+// }
+// addItem(ingredients);
+
+// const list = document.getElementById('ingredients');
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+// function addItem(...args) {
+//   for (const ingredient of ingredients) {
+//     const item = document.createElement('li');
+//     item.textContent = ingredient;
+//     list.append(item);
+//     item.classList.add('item');
+//   }
+// }
+// addItem(ingredients);
 
 // const list = document.getElementById('ingredients');
 // const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
